bdfc26dea3c7761ea0f55c18eda69f28d69f26c5
Umer Arif <umerarif01@gmail.com>
2024-10-08 04:11:05 +0500

    added: runtime export in middleware

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,7 @@
 export { auth as middleware } from "auth";
 
+export const runtime = "experimental-edge";
+
 // Read more: https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
