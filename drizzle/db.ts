d88dde63e0ab8283dbce132679e5e3a0d8bff071
Umer Arif <umerarif01@gmail.com>
2024-12-13 03:35:42 +0500

    fixed bug

--- drizzle/db.ts
+++ drizzle/db.ts
@@ -3,8 +3,6 @@ import { neon, neonConfig } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 
-config({ path: ".env" });
-
 // neonConfig.fetchConnectionCache = true;
 
 const sql = neon(process.env.DATABASE_URL!);
